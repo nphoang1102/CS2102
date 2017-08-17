234529b04c2d8c74d3116c15bdf9c27294f05d58
Hoang Nguyen <nphoang1102@gmail.com>
2017-08-17 12:21:57 -0700

    finally get the linkedlist thing up and running, dammit java

--- Homework2/src/Examples.java
+++ Homework2/src/Examples.java
@@ -1,21 +1,44 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import java.util.LinkedList;
 import org.junit.Test;
 
 public class Examples {
 
     public Examples(){}
- 
+    
+    boolean dummy = false;
+    
     // Creating some biathlon rounds
-    BiathlonRound br1 = new BiathlonRound(1, 0.5); // 240.5
-    BiathlonRound br2 = new BiathlonRound(3, 5.0); // 125
-    BiathlonRound br3 = new BiathlonRound(2, 1.2); // 181.2
-    BiathlonRound br4 = new BiathlonRound(5, 3.0); // 3
-    BiathlonRound br5 = new BiathlonRound(4, 9.0); // 69
+    static BiathlonRound br1 = new BiathlonRound(1, 0.5); // 240.5
+    static BiathlonRound br2 = new BiathlonRound(3, 5.0); // 125
+    static BiathlonRound br3 = new BiathlonRound(2, 1.2); // 181.2
+    static BiathlonRound br4 = new BiathlonRound(5, 3.0); // 3
+    static BiathlonRound br5 = new BiathlonRound(4, 9.0); // 69
 
+    // Creating some linked list
+    static LinkedList<BiathlonRound> rounds1 = new LinkedList<BiathlonRound>();
+    static {
+    	rounds1.add(br1);
+    	rounds1.add(br2);
+    	rounds1.add(br3);
+    }
+    static LinkedList<BiathlonRound> rounds2 = new LinkedList<BiathlonRound>();
+    static {
+    	rounds2.add(br3);
+    	rounds2.add(br4);
+    	rounds2.add(br5);
+    }
+    static LinkedList<BiathlonRound> rounds3 = new LinkedList<BiathlonRound>();
+    static {
+    	rounds3.add(br2);
+    	rounds3.add(br4);
+    	rounds3.add(br1);
+    }
+    
     // Creating some biathlon result
-    BiathlonResult bResult1 = new BiathlonResult(br1, br2, br3); // 546.7
-    BiathlonResult bResult2 = new BiathlonResult(br3, br4, br5); // 253.2
-    BiathlonResult bResult3 = new BiathlonResult(br2, br4, br1); // 368.5
+    BiathlonResult bResult1 = new BiathlonResult(rounds1); // 546.7
+    BiathlonResult bResult2 = new BiathlonResult(rounds2); // 253.2
+    BiathlonResult bResult3 = new BiathlonResult(rounds3); // 368.5
     
     // Creating some cycling result
     CyclingResult cResult1 = new CyclingResult(2.2, 6); // 2.2
